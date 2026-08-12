64cce6812314acc75137c81a9742b6d272a5151d
Linus Lindgren <linus.c.lindgren@gmail.com>
2026-08-12 15:14:02 +0200

    chore(eslint): upgrade eslint to v9, update config (use config imports) to avoid circular FlatCompat, and fix small lint issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import { SiteLayout } from "@/components/SiteLayout";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const dmSans = DM_Sans({
